d0a22236641f4af942613ef57be12731d810f2be
Chirag Bhatia <chiragbhatia.here2005@gmail.com>
2025-01-16 16:44:29 +0530

    basic setup mongoose connect error handller and wrapper done

---
+++ src/utils/ErrorHandler.js
@@ -0,0 +1,10 @@
+class ErrorHandler extends Error {
+  constructor(statusCode, message='Something went wrong',errors=[],stack) {
+    super(message); // parent creation is there
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.stack = stack;
+    this.success=false;
+  }
+}
